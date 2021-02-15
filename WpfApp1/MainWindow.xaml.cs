7a3e13bd5c2057cb39181ad7684c20459fc3e6de
Monoloki <62743722+Monoloki@users.noreply.github.com>
2021-02-15 19:38:42 +0100

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string zalogowany;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                using (var b = new LoginEntities())
+                {
+                    var query = b.Main
+                                              .Where(l => l.Login == Login1.Text.ToString())
+                                              .FirstOrDefault();
+                    if (query == null)
+                    {
+                    MessageBox.Show("Nieprawidłowy Login", "Error");
+                    }
+                    else
+                    {  
+                        string y = query.Login;
+                    y = String.Concat(y.Where(c => !Char.IsWhiteSpace(c)));
+                    if (y == Login1.Text.ToString())
+                        {
+                        string ea = query.Haslo;
+                        ea = String.Concat(ea.Where(c => !Char.IsWhiteSpace(c)));
+                        if (ea == Password.Text.ToString())
+                            {
+                            CharacterSelect characterSelect = new CharacterSelect();
+                            characterSelect.Show();
+                            this.Close();
+                            }
+                            else
+                            {
+                            MessageBox.Show("Nieprawidłowe Hasło", "Error");
+                            }
+                        }
+                    }
+                }
+        }
+           
+    }
+}
+
